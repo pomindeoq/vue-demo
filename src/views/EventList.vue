7d844e1015b75be73ccb1aa2464e432300bbc1f1
Mindaugas Dirma <mindaugas.dirma@gmail.com>
2020-04-15 23:48:24 +0300

    new

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -1,8 +1,31 @@
 <template>
   <div>
     <h1>Events listing</h1>
-    <router-link :to="{ name: 'event-show', params: { id: '1' } }"
-      >Show Event #1</router-link
-    >
+    <EventCard v-for="event in events" :key="event.id" :event="event" />
   </div>
 </template>
+<script>
+import EventCard from '@/components/EventCard.vue'
+import axios from 'axios'
+
+export default {
+  components: {
+    EventCard
+  },
+  data() {
+    return {
+      events: []
+    }
+  },
+  created() {
+    axios
+      .get('http://localhost:3000/events') // Does a get request
+      .then(response => {
+        this.events = response.data // For now, logs out the response
+      })
+      .catch(error => {
+        console.log('There was an error:', error.response) // Logs out the error
+      })
+  }
+}
+</script>
