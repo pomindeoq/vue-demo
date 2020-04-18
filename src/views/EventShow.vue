214aa1f4226bec1926a240409e2cd0d8f6449260
Mindaugas Dirma <mindaugas.dirma@gmail.com>
2020-04-18 21:18:10 +0300

    added create event

--- src/views/EventShow.vue
+++ src/views/EventShow.vue
@@ -3,16 +3,22 @@
     <div class="event-header">
       <span class="eyebrow">@{{ event.time }} on {{ event.date }}</span>
       <h1 class="title">{{ event.title }}</h1>
-      <h5>Organized by {{ event.organizer }}</h5>
+      <h5>Organized by {{ event.organizer ? event.organizer.name : '' }}</h5>
       <h5>Category: {{ event.category }}</h5>
     </div>
+
     <BaseIcon name="map"><h2>Location</h2></BaseIcon>
+
     <address>{{ event.location }}</address>
+
     <h2>Event details</h2>
     <p>{{ event.description }}</p>
+
     <h2>
       Attendees
-      <span class="badge -fill-gradient">{{ event.attendees.length }}</span>
+      <span class="badge -fill-gradient">{{
+        event.attendees ? event.attendees.length : 0
+      }}</span>
     </h2>
     <ul class="list-group">
       <li
@@ -27,6 +33,7 @@
 </template>
 <script>
 import EventService from '@/services/EventService.js'
+
 export default {
   props: ['id'],
   data() {
@@ -35,7 +42,7 @@ export default {
     }
   },
   created() {
-    EventService.getEvent(this.id) // <--- Send the prop id to our EventService
+    EventService.getEvent(this.id)
       .then(response => {
         this.event = response.data
       })
